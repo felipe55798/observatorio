4c495dd07b3c90f429b4a9d08cc37e878e13d2a2
felipe55798 <daniel.garciaa@autonoma.edu.co>
2020-05-13 17:01:18 -0500

    Manejo de errores 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
         ]);
 
         if($validator->fails()){
-                return response()->json($validator->errors()->toJson(), 400);
+                return response()->json($validator->errors()->toJson());
         }
 
         $user = User::create([
